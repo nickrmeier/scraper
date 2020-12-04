674742957b91243cde1d5075df1d4d9c612924f5
nickrmeier <nickrmeier@gmaill.com>
2020-12-03 16:51:32 -0800

    data is sorted by user votes

--- scrape.py
+++ scrape.py
@@ -3,10 +3,21 @@ from bs4 import BeautifulSoup
 import pprint
 
 res = requests.get('https://news.ycombinator.com/news')
+res2 = requests.get('https://news.ycombinator.com/news?p=2')
 soup = BeautifulSoup(res.text, 'html.parser')
+soup2 = BeautifulSoup(res2.text, 'html.parser')
 
 links = soup.select('.storylink')
 subtext = soup.select('.subtext')
+links2 = soup2.select('.storylink')
+subtext2 = soup2.select('.subtext')
+
+mega_link = links + links2
+mega_subtext = subtext + subtext2
+
+
+def sort_stories_by_votes(hnlist):
+    return sorted(hnlist, key=lambda k: k['votes'], reverse=True)
 
 
 def create_custom_hn(link, subtext):
@@ -20,7 +31,7 @@ def create_custom_hn(link, subtext):
             if points > 99:
                 hn.append({'title': title, 'link': href, 'votes': points})
 
-    return hn
+    return sort_stories_by_votes(hn)
 
 
-pprint.pprint(create_custom_hn(links, subtext))
+pprint.pprint(create_custom_hn(mega_link, mega_subtext))
